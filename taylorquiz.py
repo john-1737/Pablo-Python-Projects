5452869cb681311eeb283c62f0a6f1fff2c91600
john-1737 <bellows.john@gmail.com>
2024-07-01 01:55:29 +0000

    code on 6/30/2024

---
+++ taylorquiz.py
@@ -0,0 +1,8 @@
+def gamestart():
+    print("It's time for a Taylor Swift quiz!\nHere are some instructions for the quiz:")
+    print("On open-ended questions, type your answer.\nOn multiple-choice questions, type the letter of your choice.\nOn true or false questions, enter T for true and F for false.\nYou get one point per question. There are 7 questions.")
+def quiz():
+    pass
+gamestart()
+if input('Type "go" to begin!\n') == 'go':
+    quiz()  
